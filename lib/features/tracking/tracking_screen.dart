be60f944f12e43ca4ad50562ffb3be5266ba4ed0
healy233 <11775331+healy233@user.noreply.gitee.com>
2026-08-28 07:38:50 -0700

    fix: use white weight history sheet

--- lib/features/tracking/tracking_screen.dart
+++ lib/features/tracking/tracking_screen.dart
@@ -399,6 +399,7 @@ class _TrackingScreenState extends ConsumerState<TrackingScreen> {
       context: context,
       isScrollControlled: true,
       showDragHandle: true,
+      backgroundColor: Colors.white,
       builder:
           (sheetContext) => SafeArea(
             child: Padding(
